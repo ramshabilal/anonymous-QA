ec3e846172a4a58dbc6b05bf70fcb3aa62fdd983
ramshabilal <rb4919@nyu.edu>
2023-12-06 18:22:13 -0500

    setting up

--- src/db.mjs
+++ src/db.mjs
@@ -1,4 +1,7 @@
+import './config.mjs';
 import mongoose from 'mongoose'
+
+console.log(process.env.DSN)
 
 const QuestionSchema = new mongoose.Schema({
   question: { type: String, required: true },
@@ -6,7 +9,9 @@ const QuestionSchema = new mongoose.Schema({
 })
 
 console.log('connecting to database', process.env.DSN)
-mongoose.connect(process.env.DSN)
+let DSN = 'mongodb+srv://ramshabilal:RsRRPoY9gZCVNjhi@cluster0.siam2zv.mongodb.net/hw06?retryWrites=true&w=majority';
+//mongoose.connect(process.env.DSN)
+mongoose.connect(DSN); 
 
 const Question = mongoose.model("Questions", QuestionSchema)
 export default Question
